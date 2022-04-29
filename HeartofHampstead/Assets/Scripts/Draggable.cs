bc80441d72477e6971abe2379d87d1bb6ff2e5b2
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-04-29 08:42:32 -0500

    Final Decision Screen/Builds

--- HeartofHampstead/Assets/Scripts/Draggable.cs
+++ HeartofHampstead/Assets/Scripts/Draggable.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour, IDragHandler
 {
+    /*
     [SerializeField]
     private float dampingSpeed = .05f;
 
@@ -18,10 +20,30 @@ public class Draggable : MonoBehaviour, IDragHandler
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-       if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObject, eventData.position, eventData.pressEventCamera, out var globalMousePosition))
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(draggingObject, eventData.position, eventData.pressEventCamera, out var  globalMousePosition))
         {
             draggingObject.position = Vector3.SmoothDamp(draggingObject.position, globalMousePosition, ref velocity, dampingSpeed);
         }
     }
+    */
+
+    private Vector3 _startPos;
+
+    void Awake()
+    {
+        _startPos = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        transform.position = UnityEngine.InputSystem.Mouse.current.position.ReadValue() - new Vector2(0, 20);
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        //transform.position = _startPos;
+    }
+
 
 }
